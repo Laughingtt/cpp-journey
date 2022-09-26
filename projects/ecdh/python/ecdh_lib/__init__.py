2f95bf9c6ba6d22ae5ac48240cee05c77e70d947
tianjian <jian.tian@udai.work>
2022-09-26 09:54:32 +0800

    update setup

---
+++ projects/ecdh/python/ecdh_lib/__init__.py
@@ -0,0 +1,15 @@
+import _ecdh_lib
+
+Test = _ecdh_lib.Test
+EccEncrypt = _ecdh_lib.EccEncrypt
+EccObliviousTransfer = _ecdh_lib.EccObliviousTransfer
+ECC = _ecdh_lib.ECC
+HashString = _ecdh_lib.HashString
+
+__all__ = [
+    "Test",
+    "EccEncrypt",
+    "EccObliviousTransfer",
+    "ECC",
+    "HashString"
+]
